6575ed3df74abdc20cbaeb07cc319c5e38a5a1b0
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2021-01-13 23:58:43 +0530

    DOM Table element structure creation in JS

--- fetchusers/script.js
+++ fetchusers/script.js
@@ -1,23 +1,66 @@
 const root = document.querySelector('#root')
 const fetchbutton = document.querySelector('#fetchbutton')
+const textbox = document.querySelector('#textbox')
+// const loader = document.querySelector('#loader')
 
 
 async function getUsers() {
     try {
+        root.innerHTML = 'Loading...'
         let response = await fetch('http://jsonplaceholder.typicode.com/users')
+
+        if (response.ok) {
+            root.innerHTML = ''
+        }
         const data = await response.json();
+
+        const card = document.createElement('div')
+
+        const table = document.createElement("TABLE")
+        table.style.width = '100%'
+        table.setAttribute('border', '1')
+
+        let tableheaders = ["User", "Email", "Details"]
+
+        const tablehead = document.createElement('thead')
+        const tablerow = document.createElement('tr')
+
+        tableheaders.forEach(header => {
+            const th = document.createElement('th')
+            th.innerText = header
+            tablerow.append(th)
+        })
+
+        tablehead.append(tablerow)
+        table.append(tablehead)
+
+        const tbody = document.createElement('tbody')
+
+        // root.append(table)
+
         data.forEach((user) => {
-            const card = document.createElement('div')
-        
-            const h1 = document.createElement('h1')
-            h1.textContent = user.name
-        
-            const h2 = document.createElement('h2')
-            h2.textContent = user.email
-        
-            root.appendChild(card)
-            card.appendChild(h1)
-            card.appendChild(h2)
+
+            const trow = document.createElement('tr')
+
+            const username = document.createElement('td')
+            username.innerText = user.name
+
+            const email = document.createElement('td')
+            email.innerText = user.email
+
+            const details = document.createElement('td')
+            details.innerText = 'Details'
+
+            trow.append(username, email, details)
+
+            tbody.append(trow)
+            table.append(tbody)
+            root.append(table)
+
+
+            details.addEventListener('click', () => {
+                textbox.value = 'UserName: ' + user.name + '\n' + 'Company: ' + user.company.name + '\n' + 'City: ' + user.address.city
+            })
         })
     } catch (error) {
         console.log(error.message);
